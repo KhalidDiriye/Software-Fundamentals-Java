d494aa3b106538a2e90ad69a8bc5bf7cc2d72524
Khalid Y Diriye <kdiriye18@gmail.com>
2018-02-21 19:14:23 -0600

    adding in more work

--- Radix.java
+++ Radix.java
@@ -1,49 +1,74 @@
 import queuepackage.*;
 
-public class Radix {
-	public static void main(String[] args) {
-		int[] array = {143,934,782,687,555,222,111,213,842,2000};
-		printArray(array);
-		System.out.println();
-		radixSort(array,1000);
-		printArray(array);
-	}	
-	
-	public static void printArray(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			if (i < array.length - 1) {
-				System.out.print(array[i] + ",");
-			} else {
-				System.out.print(array[i]);
-			}
-		}
-	}	
-	
-	public static void radixSort(int[] array, int maxPowerOf10) {
-		Queue[] queueArray = new Queue[10];
-		
-		for (int queueNum = 0; queueNum < 10; queueNum++) {
-			queueArray[queueNum] = new Queue();
-		}
+/**
+Program sorts numbers using the radix sort algorithm 
+Author: Khalid Diriye
+*/ 
 
-		for (int powerOf10 = 1; powerOf10 <= maxPowerOf10; powerOf10 = powerOf10 * 10) {
-			for (int item = 0; item < array.length; item++) {
-				int digit = getDigit(array[item], powerOf10);
-				queueArray[digit].enqueue(new Integer(array[item]));
-			}
-			
-			int item = 0;
-			
-			for (int queueNum = 0; queueNum < 10; queueNum++) {
-				while(!queueArray[queueNum].isEmpty()) {
-					array[item] = ((Integer) queueArray[queueNum].dequeue()).intValue();
-					item++;
-				}	
-			}	
-		}
-	}	
-	
-	public static int getDigit(int number, int powerOf10) {
-		return (number/powerOf10)%10;
-	}	
-}	
+import java.util.Random;
+
+public class RadixSort {
+
+    public static void main(String[] args){
+        final int SIZE = 1000000;
+
+        Random r = new Random();
+        int[] test = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++){
+            test[i] = r.nextInt(Integer.MAX_VALUE);
+        }
+
+        long start = System.currentTimeMillis();
+        test = sort(test);
+        long end = System.currentTimeMillis();
+
+        for (Integer i : test){
+            System.out.println(i);
+        }
+
+        System.out.println(end-start);
+    }
+
+    // Sort the numbers beginning with least-significant digit
+    public static int[] sort(int[] input){
+
+        // Largest place for a 32-bit int is the 1 billion's place
+        for(int place=1; place <= 1000000000; place *= 10){
+            // Use counting sort at each digit's place
+            input = countingSort(input, place);
+        }
+
+        return input;
+    }
+
+    private static int[] countingSort(int[] input, int place){
+        int[] out = new int[input.length];
+
+        int[] count = new int[10];
+
+        for(int i=0; i < input.length; i++){
+            int digit = getDigit(input[i], place);
+            count[digit] += 1;
+        }
+
+        for(int i=1; i < count.length; i++){
+            count[i] += count[i-1];
+        }
+
+        for(int i = input.length-1; i >= 0; i--){
+            int digit = getDigit(input[i], place);
+
+            out[count[digit]-1] = input[i];
+            count[digit]--;
+        }
+
+        return out;
+
+    }
+
+    private static int getDigit(int value, int digitPlace){
+        return ((value/digitPlace ) % 10);
+    }
+
+}
